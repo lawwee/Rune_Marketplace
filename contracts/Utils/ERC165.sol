ab93956a7cf3028527f18c518c068a12beb228a6
Lawwee <lawalmohammed567@gmail.com>
2022-10-01 00:23:35 +0100

    changed compiler to 0.8.10 to fully support hardhat

--- contracts/Utils/ERC165.sol
+++ contracts/Utils/ERC165.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.10;
 
-import "../interfaces/IERC165.sol";
+import "../Interfaces/IERC165.sol";
 
 abstract contract ERC165 is IERC165 {
     function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
