c610e98ea3ebe667b85bad7627aabd1883bc0df2
Lawwee <lawalmohammed567@gmail.com>
2022-10-01 13:24:54 +0100

    created uri storage implementation for ERC721

---
+++ contracts/Utils/ERC721URIStorage.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.10;
+
+import "./ERC721.sol";
+
+abstract contract ERC721URIStorage is ERC721 {
+    using Strings for uint256;
+
+    mapping(uint256 => string) private _tokenURIs;
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireMinted(tokenId);
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+
+        if (bytes(base).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        require(_exists(tokenId), "ERC721URIStorage: token ID does not exist");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function _burn(uint256 tokenId) internal override {
+        super._burn(tokenId);
+
+        if (bytes(_tokenURIs[tokenId]).length > 0) {
+            delete _tokenURIs[tokenId];
+        }
+    }
+}
