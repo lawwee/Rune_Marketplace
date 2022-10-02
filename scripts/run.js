36f2f715f5e8425f2e426184ed37df61bbabab1e
Lawwee <lawalmohammed567@gmail.com>
2022-10-02 04:44:18 +0100

    script to test all core and basic functionalities

--- scripts/run.js
+++ scripts/run.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 const baseURI = "https://gateway.pinata.cloud/ipfs/QmcwdWvZB6nqoCFDw4CKU2vZYC5oHJPtAr6ehCV2EMBTkW/"
 
 async function main() {
-    const [owner, another] = await hre.ethers.getSigners()
+    const [owner, another, randomUser] = await hre.ethers.getSigners()
     const runeContractFactory = await hre.ethers.getContractFactory("RuneNFT")
     const runeContract = await runeContractFactory.deploy(baseURI)
     await runeContract.deployed()
@@ -11,8 +11,34 @@ async function main() {
     let txn = await runeContract.getPrice()
     console.log(txn)
 
+    txn = await runeContract.name()
+    console.log(txn)
+
+    txn = await runeContract.symbol()
+    console.log(txn)
+
     txn = await runeContract.connect(another).mintNFT("Odin", {value: hre.ethers.utils.parseEther("0.0001")})
     await txn.wait()
+
+    txn = await runeContract.ownerOf(0)
+    console.log(txn)
+
+    txn = await runeContract.connect(another).approve(owner.address, 0)
+    await txn.wait()
+    console.log("approval successful")
+
+    txn = await runeContract.connect(another).setApprovalForAll(owner.address, true)
+    await txn.wait()
+    console.log("total approval done")
+
+    txn = await runeContract.isApprovedForAll(another.address, owner.address)
+    console.log(txn)
+
+    txn = await runeContract.getApproved(0)
+    console.log(txn)
+
+    txn = await runeContract.balanceOf(another.address)
+    console.log(txn)
     
     txn = await runeContract.tokenURI(0)
     console.log(txn)
@@ -21,6 +47,13 @@ async function main() {
     console.log(txn)
 
     txn = await runeContract.mintedSingleRune("Odin")
+    console.log(txn)
+
+    txn = await runeContract.transferFrom(another.address, randomUser.address, 0)
+    await txn.wait()
+    console.log("transfer successful")
+
+    txn = await runeContract.ownerOf(0)
     console.log(txn)
 
 }
