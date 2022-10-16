cd5bd2f719039265da8fb1379d46d01a180677bc
Lawwee <lawalmohammed567@gmail.com>
2022-10-16 18:05:38 +0100

    started script test

--- scripts/run.js
+++ scripts/run.js
@@ -1,34 +1,23 @@
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 const baseURI = "https://gateway.pinata.cloud/ipfs/QmcwdWvZB6nqoCFDw4CKU2vZYC5oHJPtAr6ehCV2EMBTkW/"
 
 async function main() {
-    // const [owner, another, randomUser] = await hre.ethers.getSigners()
+    const [owner, another, randomUser] = await hre.ethers.getSigners()
     const runeContractFactory = await hre.ethers.getContractFactory("Auction")
     const runeContract = await runeContractFactory.deploy(baseURI)
     await runeContract.deployed()
 
-    txn = await runeContract.mintNFT("Odin", {value: hre.ethers.utils.parseEther("0.0001")})
+    let balance = await hre.ethers.provider.getBalance(runeContract.address);
+    console.log("Another Balance",hre.ethers.utils.formatEther(balance));
+
+    txn = await runeContract.connect(another).mintNFT("Odin", {value: hre.ethers.utils.parseEther("0.0001")})
     await txn.wait()
 
-    txn = await runeContract.ownerOf(0)
-    console.log(txn)
+    balance = await hre.ethers.provider.getBalance(runeContract.address);
+    console.log("Another Balance", hre.ethers.utils.formatEther(balance));
 
-    txn = await runeContract.currentBid(0)
-    console.log(txn)
-
-    txn = await runeContract.highestBidder(0)
-    console.log(txn)
-
-    txn = await runeContract.setStartBid(0, 2)
-    await txn.wait()
-    console.log("Set success")
-
-    txn = await runeContract.currentBid(0)
-    console.log(txn)
-
-    txn = await runeContract.highestBidder(0)
-    console.log(txn)
 }
 
 main().catch((error) => {
