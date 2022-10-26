8d2020c450b321ccaf618896fdd6987729da8575
Lawwee <lawalmohammed567@gmail.com>
2022-10-27 00:05:14 +0100

    late update

--- contracts/Interfaces/IERC721.sol
+++ contracts/Interfaces/IERC721.sol
@@ -11,8 +11,8 @@ interface IERC721 is IERC165 {
 
     function balanceOf(address owner) external view returns (uint256 balance);
     function ownerOf(uint256 tokenId) external view returns (address owner);
-    // re-entrancy vulnerability to safetransferfrom
     function transferFrom(address from, address to, uint256 tokenId) external;
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external ;
     function approve(address to, uint256 tokenId) external;
     function setApprovalForAll(address operator, bool _approved) external;
     function getApproved(uint256 tokenId) external view returns (address operator);
